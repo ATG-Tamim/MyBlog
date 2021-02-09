897e6e3d72d78b1e8f87309b5ea5e748f41627e9
ATG-Tamim <atg.ahmad.tamim.ghyasi@gmail.com>
2021-02-09 14:24:29 +0430

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>How are You</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import bootstrap from "bootstrap";
+import "./assets/style/style.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .mount("#app");
